26e39ed7a675cb663c91e030fa1a6195aeade304
denisdeyneka <d.deyneka89@gmail.com>
2020-09-19 22:22:08 +0300

    modules

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg' alt='' />
+      </div>
+      <div>
+        ava+descr
+        </div>
+      <div>
+        My posts
+          <div>
+          New post
+          </div>
+        <div>
+          post 1
+          </div>
+        <div>
+          post 2
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
